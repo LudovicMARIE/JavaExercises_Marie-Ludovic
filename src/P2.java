8b20bea1c2428ec0c4d7612751c8f5cb76d6172c
Ludovic MARIE <ludovic.marie@aol.com>
2023-10-02 13:25:23 +0100

    P2 exo 2 start

--- src/P2.java
+++ src/P2.java
@@ -1,6 +1,52 @@
-public class P2 {
-    public static void main(String[] args) {
-        System.out.println("test");
+import java.util.ArrayList;
 
+public class P2 {
+    private static Integer sum(ArrayList<Integer> stock) {
+        Integer total = 0;
+        for (Integer number : stock) {
+            total += number;
+        }
+        return total;
+    }
+    private static int findFruit(ArrayList<String> tab, String str) {
+        for (int i = 0; i < tab.size(); i++) {
+            if (tab.get(i).equals(str)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+
+        //Exercice 1
+        ArrayList<Integer> stock = new ArrayList<Integer>();
+        stock.add(14);
+        stock.add(887);
+        stock.add(58);
+        stock.add(713);
+        stock.add(179);
+        stock.add(512);
+        stock.add(786);
+        stock.add(29);
+
+        System.out.println("Contenu du stock : "+sum(stock));
+
+
+
+        //Exercice 2
+        ArrayList<String> tab = new ArrayList<String>();
+        tab.add("kiwi");
+        tab.add("pomme");
+        tab.add("poire");
+        tab.add("litchi");
+        tab.add("rhubarbe");
+
+        int findIndex = findFruit(tab, args[0]);
+        if (findIndex == -1) {
+            System.out.println("Le fruit " + args[0] + " n'est pas dans la liste.");
+        } else {
+            System.out.println("Le fruit " + args[0] + " est à l'index " + findIndex + " dans la liste.");
+        }
     }
 }
---
+++ src/P2_Exo3.java
@@ -0,0 +1,5 @@
+public class P2_Exo3 {
+    public static void main(String[] args) {
+        
+    }
+}
