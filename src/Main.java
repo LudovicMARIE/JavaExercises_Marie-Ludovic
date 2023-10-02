68672691223652e4c29dc0ae125f6675a4fa1c5f
Ludovic MARIE <ludovic.marie@aol.com>
2023-10-02 08:49:28 +0100

    project create

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer i = 0;
+        String s = "text";
+        double d = 0.1;
+        System.out.print("Test");
+
+    }
+}
